ed65ea75b982f44277846724e29b4472878f8824
Alexandre <161913260+AlexandreLeonez@users.noreply.github.com>
2024-09-06 22:00:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Neymar",
+        descricao: "Neymar, joga muito",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "tem taleto"
+    },
+    {
+        titulo: "CR7",
+        descricao: "CR& joga muito mas perde pro messi",
+        link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+        tags: "ciborgue"
+    },
+    {
+        titulo: "Messi",
+        descricao: "Messi brabo melhor q CR7",
+        link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+        tags: "joga massa"
+    }
+];
